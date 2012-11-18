5acba86c212fc462381adfcfa869ef2c68321ba4
fukayatsu <fukayatsu@gmail.com>
2012-11-18 15:28:50 +0900

    カーソル行の取得

--- SublimeTweetLine.py
+++ SublimeTweetLine.py
@@ -5,4 +5,7 @@ import sublime_plugin
 class TweetLineCommand(sublime_plugin.TextCommand):
     # ^ + ` and view.run_command('tweet_line')
     def run(self, edit):
-        self.view.insert(edit, 0, "TweetLine!")
+        for region in self.view.sel():
+            # region_of_line = self.view.full_line(region)  # 改行を含む
+            region_of_line = self.view.full_line(region)
+            print self.view.substr(region_of_line)
