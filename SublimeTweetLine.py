d5156f820f93ab07f062e706e59888639a20bc92
fukayatsu <fukayatsu@gmail.com>
2012-11-18 15:47:52 +0900

    確認ダイアログの表示・行頭の空白文字を無視

--- SublimeTweetLine.py
+++ SublimeTweetLine.py
@@ -1,11 +1,17 @@
-#import sublime
+import sublime
 import sublime_plugin
+import re
 
 
 class TweetLineCommand(sublime_plugin.TextCommand):
     # ^ + ` and view.run_command('tweet_line')
     def run(self, edit):
-        for region in self.view.sel():
-            # region_of_line = self.view.full_line(region)  # 改行を含む
-            region_of_line = self.view.full_line(region)
-            print self.view.substr(region_of_line)
+        first_region = self.view.sel()[0]
+        # region_of_line = self.view.full_line(region)  # including line end
+        region_of_line = self.view.line(first_region)
+        line = self.view.substr(region_of_line)
+        tweet_string = re.sub(r"^\s+", "", line)
+        if sublime.ok_cancel_dialog("Tweet this?\n\n" + tweet_string, "Tweet"):
+            print line
+        else:
+            print "Cancel"
