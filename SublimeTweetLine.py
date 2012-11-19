76e1fbf8dee33c592489f0e85257e914d6346e79
fukayatsu <fukayatsu@gmail.com>
2012-11-20 02:26:53 +0900

    add authorization

--- SublimeTweetLine.py
+++ SublimeTweetLine.py
@@ -4,21 +4,27 @@ import sublime
 import sublime_plugin
 import re
 import libs.twitter as twitter
-
-# import json
-# import thread
-api = twitter.Api(
-    consumer_key        = '1uFNM4QtiqRGB1ZQGKUY8g',
-    consumer_secret     = 'BeCH5j8ZPmum357xEF9tvVf1VttVWY2E8hpcfk0',
-    access_token_key    = 'YOUR_TOKEN_HERE',
-    access_token_secret = 'YOUR_SECRET_HERE',
-    input_encoding      = 'utf8'
-)
+import libs.AccessTokenFactory as factory
+import json
 
 
 class TweetLineCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+
+        self.settings = Settings()
+        if not self.settings.settings['token_key']:
+            self.view.window().run_command('input_pincode')
+            return
+
+        api = twitter.Api(
+            consumer_key        = '1uFNM4QtiqRGB1ZQGKUY8g',
+            consumer_secret     = 'BeCH5j8ZPmum357xEF9tvVf1VttVWY2E8hpcfk0',
+            access_token_key    = self.settings.settings['token_key'],
+            access_token_secret = self.settings.settings['token_secret'],
+            input_encoding      = 'utf8'
+        )
+
         first_region = self.view.sel()[0]
         # region_of_line = self.view.full_line(region)  # including line end
         region_of_line = self.view.line(first_region)
@@ -36,3 +42,56 @@ class TweetLineCommand(sublime_plugin.TextCommand):
                 sublime.status_message('tweet error :(')
         else:
             print "Cancel"
+
+
+class InputPincodeCommand(sublime_plugin.WindowCommand):
+    def __init__(self, window):
+        self.window = window
+        self.settings = Settings()
+
+    def run(self):
+        self.window.show_input_panel('input pincode:', '', self.on_input_pin, None, None)
+        self.tokenFactory = factory.AccessTokenFactory()
+        import webbrowser
+        webbrowser.open(self.tokenFactory.getTempToken())
+
+    def on_input_pin(self, text):
+        try:
+            pincode = int(text)
+            keys = self.tokenFactory.getAccessToken(pincode)
+        except ValueError:
+            print 'Invalid pincode?, try again'
+            return
+        except:
+            print 'Error, try again please'
+            sublime.status_message('Error, try again please')
+            return
+
+        self.access_token_key, self.access_token_secret = keys
+        self.settings.settings['token_key'] = self.access_token_key
+        self.settings.settings['token_secret'] = self.access_token_secret
+        self.settings.saveAll()
+        sublime.status_message('You are authorized!')
+
+class Settings:
+    def __init__(self, filename='SublimeTweetLine.settings'):
+        self.defaults = {
+            'token_key': None,
+            'token_secret': None,
+        }
+        self.filename = sublime.packages_path() + '/User/' + filename
+        self.settings = self.loadAll()
+
+    def loadAll(self):
+        try:
+            setting_json = open(self.filename).read()
+            return json.loads(setting_json)
+        except:
+            return self.defaults
+
+    def saveAll(self):
+        try:
+            with open(self.filename, 'w') as f:
+                f.write(json.dumps(self.settings, sort_keys=True, indent=2))
+        except:
+            print 'save error'
---
+++ libs/AccessTokenFactory.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python2.4
+#
+# Copyright 2007 The Python-Twitter Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import sys
+
+# parse_qsl moved to urlparse module in v2.6
+try:
+  from urlparse import parse_qsl
+except:
+  from cgi import parse_qsl
+
+import oauth2 as oauth
+
+class AccessTokenFactory:
+  def __init__(self):
+    self.consumer_key      = '1uFNM4QtiqRGB1ZQGKUY8g'
+    self.consumer_secret   = 'BeCH5j8ZPmum357xEF9tvVf1VttVWY2E8hpcfk0'
+    self.REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+    self.ACCESS_TOKEN_URL  = 'https://api.twitter.com/oauth/access_token'
+    self.AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+    self.SIGNIN_URL        = 'https://api.twitter.com/oauth/authenticate'
+
+  def getTempToken(self):
+    self.signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+    self.oauth_consumer             = oauth.Consumer(key=self.consumer_key, secret=self.consumer_secret)
+    self.oauth_client               = oauth.Client(self.oauth_consumer)
+
+    self.res, self.content = self.oauth_client.request(self.REQUEST_TOKEN_URL, 'GET')
+
+    if self.res['status'] != '200':
+      print 'Invalid respond from Twitter requesting temp token: %s' % self.res['status']
+    else:
+      self.request_token = dict(parse_qsl(self.content))
+      return '%s?oauth_token=%s' % (self.AUTHORIZATION_URL, self.request_token['oauth_token'])
+
+
+  def getAccessToken(self, pin):
+    token = oauth.Token(self.request_token['oauth_token'], self.request_token['oauth_token_secret'])
+    token.set_verifier(pin)
+
+    self.oauth_client  = oauth.Client(self.oauth_consumer, token=token)
+    res, content = self.oauth_client.request(self.ACCESS_TOKEN_URL, method='POST', body='oauth_verifier=%s' % pin)
+    access_token  = dict(parse_qsl(content))
+
+    # print self.request_token['oauth_token']
+    if res['status'] != '200':
+      print 'Error status: %s' % res['status']
+      raise
+    else:
+      return (access_token['oauth_token'], access_token['oauth_token_secret'])
+
