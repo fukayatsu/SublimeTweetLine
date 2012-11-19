e366a18e49ad08518ee9436b164c1de5f678b1a2
fukayatsu <fukayatsu@gmail.com>
2012-11-19 16:21:34 +0900

    bug fix

--- SublimeTweetLine.py
+++ SublimeTweetLine.py
@@ -7,15 +7,16 @@ import libs.twitter as twitter
 
 # import json
 # import thread
+api = twitter.Api(
+    consumer_key        = '1uFNM4QtiqRGB1ZQGKUY8g',
+    consumer_secret     = 'BeCH5j8ZPmum357xEF9tvVf1VttVWY2E8hpcfk0',
+    access_token_key    = 'YOUR_TOKEN_HERE',
+    access_token_secret = 'YOUR_SECRET_HERE',
+    input_encoding      = 'utf8'
+)
+
 
 class TweetLineCommand(sublime_plugin.TextCommand):
-    api = twitter.Api(
-        consumer_key        = '1uFNM4QtiqRGB1ZQGKUY8g',
-        consumer_secret     = 'BeCH5j8ZPmum357xEF9tvVf1VttVWY2E8hpcfk0',
-        access_token_key    = 'YOUR_TOKEN_HERE',
-        access_token_secret = 'YOUR_SECRET_HERE',
-        input_encoding      = 'utf8'
-    )
 
     def run(self, edit):
         first_region = self.view.sel()[0]
